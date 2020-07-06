271a4daff835c8c2c3d43e992eae125490eed305
Michael <michalbaz@poczta.onet.pl>
2020-07-06 15:26:34 +0200

    pop push and print_data working properly

--- stack/main.c
+++ stack/main.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#define STR_SIZE 20
 
 typedef struct{
     int id;
-    char fname[20];
-    char lname[20];
+    char fname[STR_SIZE];
+    char lname[STR_SIZE];
 }data_t;
 
 typedef struct{
@@ -32,29 +33,53 @@ const char* menu_text =
 "[6].End.\n";
 
 int main(void){
-    stack_t* data;
+    stack_t stack; 
     unsigned temp;
     char c;
+    data_t* pop_pointer, temp_data;
 
     printf("Pass the size of stack (size >= 1): ");
     while(scanf("%u", &temp) && temp < 1){
         printf("Pass valid size of stack: ");
     }
-    data->size = temp;
-    if((data->arr = (data_t*)malloc(sizeof(data_t) * data->size)) == NULL){
+    stack.size = temp;
+    if((stack.arr = (data_t*)malloc(sizeof(data_t) * stack.size)) == NULL){
         printf("Failed to alloc memory.\n");
         exit(1);
     }
     else{
         printf("Memory allocated successfully.\n%s", menu_text);
-        data->quantity = 0;
+        stack.quantity = 0;
         clear_in();
         while((c = getchar()) != '6'){
             clear_in();
             switch(c){
                 case '1':
+                    if(!stack_full(&stack)){
+                        printf("Pass the data.\nID: ");
+                        scanf("%d", &temp_data.id);
+                        printf("First name: ");
+                        clear_in();
+                        scanf("%19s", temp_data.fname);
+                        clear_in();
+                        printf("Last name: ");
+                        scanf("%19s", temp_data.lname);
+                        clear_in();
+                        push(&stack, &temp_data);
+                    }
+                    else{
+                        printf("Stack full.\n");
+                    }
                 break;
                 case '2':
+                    if(!stack_empty(&stack)){
+                        pop_pointer = pop(&stack);
+                        print_data(pop_pointer);
+                        free(pop_pointer);
+                    }
+                    else{
+                        printf("Stack is empty.\n");
+                    }
                 break;
                 case '3':
                 break;
@@ -62,8 +87,8 @@ int main(void){
                 break;
                 case '5':
                     printf("-- Stack --\n");
-                    printf("size: %5zu\n", data->size);
-                    printf("elem: %5zu\n", data->quantity);
+                    printf("size: %5zu\n", stack.size);
+                    printf("elem: %5zu\n", stack.quantity);
                     printf("-----------\n\n");
                 break;
                 default:
@@ -73,7 +98,7 @@ int main(void){
             }
             printf("%s", menu_text);
         }
-        free(data->arr);
+        free(stack.arr);
     }
     return 0;
 }
@@ -99,11 +124,22 @@ bool push(stack_t* stack, const data_t* data){
 }
 
 data_t* pop(stack_t* stack){
+    data_t* temp, *pt;
     if(stack_empty(stack)){
         return NULL;
     }
     else{
         
+        if((temp = (data_t*)malloc(sizeof(data_t) * 1)) == NULL){
+            return NULL;
+        }
+        else{
+            pt = &stack->arr[--stack->quantity];
+            temp->id = pt->id;
+            strcpy(temp->fname, pt->fname);
+            strcpy(temp->lname, pt->lname);
+            return temp;
+        }
     }
 }
 bool stack_full(const stack_t* stack){
