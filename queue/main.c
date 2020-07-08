f2a42081e8b3cd311426cfab411aafb676466dca
Michael <michalbaz@poczta.onet.pl>
2020-07-08 13:07:40 +0200

    basic data structures and function prototypes implemented

--- queue/main.c
+++ queue/main.c
@@ -1,3 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct{
+    int number; //some random data to be stored
+    char string[40];
+    data_t* next; //pointer to the next element 
+}data_t;
+
+typedef struct{
+    size_t size; //max size of the queue
+    size_t quantity; //elements stored in queue
+    data_t* head; //pointer to the head of the queue
+}queue_t;
+
+bool enqueue(queue_t* queue, const data_t* element); //adds an element to our data structure
+data_t* dequeue(queue_t* queue); //pops first element from queue and returns pointer to newly allocated memory
+bool queue_empty(const queue_t* queue); //checks if queue is empty
+bool queue_full(const queue_t* queue); //checks if queue is full
+
 int main(void){
 
     return 0;
